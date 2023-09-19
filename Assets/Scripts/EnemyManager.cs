2cb9a1ae1c7d212795a26ce416c2d1ee964919bb
Jan <janaalto96@gmail.com>
2023-09-19 09:14:27 +0300

    korjaus

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int enemyAmount = 5;
+    public int enemySpacing = 7;
+    public GameObject enemyPrefab;
+    public List<GameObject> enemyList = new List<GameObject>();
+
+    public float enemySpeed = 15f;
+    public bool enemySide = false;
+    private float sideMultiplier = 1f;
+    public int boundary = 35;
+
     void Start()
     {
-        
+        for (int i = 0; i < enemyAmount; i++)
+        {
+            GameObject spawnedEnemy = Instantiate(enemyPrefab, this.transform);
+            spawnedEnemy.transform.position = new Vector3(enemySpacing * i, 31, 0);
+            enemyList.Add(spawnedEnemy);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
-        
+        bool check = PositionChecker(enemyList);
+        if (check == false)
+        {
+            sideMultiplier = sideMultiplier * -1;
+        }
+        foreach (GameObject moveEnemy in enemyList)
+        {
+            moveEnemy.transform.Translate(Vector3.left * enemySpeed * sideMultiplier * Time.deltaTime);
+        }
+    }
+
+    private bool PositionChecker(List<GameObject> eList)
+    {
+        bool result = true;
+        foreach (GameObject Enemy in eList)
+        {
+            if (Enemy.transform.position.x > boundary || Enemy.transform.position.x < -boundary)
+            {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
     }
 }
+
